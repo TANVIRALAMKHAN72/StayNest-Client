a59276806d5d6a7d050c7165792fabf9fe043e05
TANVIRALAMKHAN72 <tanviralamkhantonmoy@gmail.com>
2025-07-21 07:57:45 +0600

    first project

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,59 +1,4 @@
-// import {
-//   createBrowserRouter,
-// } from "react-router";
-// import RootLayout from "../Layout/RootLayout";
-// import HomePage from "../Pages/HomePage";
-// import Login from "../Pages/Login";
-// import Register from "../Pages/Register";
-// import Apartment from "../Component/Apartment";
-// import ApplyAgreement from "../Pages/ApplyAgreement";
-// import PrivateRoute from "../Routes/PrivateRoute";
-// import ErrorPage from "../Pages/ErrorPage";
-
-
-
-// export const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <RootLayout></RootLayout>,
-//     children: [
-//         {
-//             index: true,
-//             element: <HomePage></HomePage>,
-//         },
-//         {
-//           path: '/apartment',
-//           element: <Apartment></Apartment>
-//         },
-//        {
-//        path: '/apply-agreement/:id',
-//         element: (
-//           <PrivateRoute>
-//             <ApplyAgreement />
-//           </PrivateRoute>
-//         ),
-//       },
-//     ],
-//   },
-//   {
-//     path:"/login",
-//     element: <Login></Login>,
-//   },
-//   {
-//     path: "/register",
-//     element: <Register></Register>,
-//   },
-//   {
-//     path: "*",
-//     element: <ErrorPage></ErrorPage>,
-//   }
-// ]);
-
-
-
-
-// src/router/router.jsx
-import { createBrowserRouter } from "react-router"; // react-router-dom থেকে
+import { createBrowserRouter } from "react-router";
 import RootLayout from "../Layout/RootLayout";
 import HomePage from "../Pages/HomePage";
 import Login from "../Pages/Login";
@@ -62,17 +7,8 @@ import Apartment from "../Component/Apartment";
 import ApplyAgreement from "../Pages/ApplyAgreement";
 import PrivateRoute from "../Routes/PrivateRoute";
 import ErrorPage from "../Pages/ErrorPage";
-// নতুন ড্যাশবোর্ড লেআউট
- // এই পেজগুলো পরে তৈরি করতে হবে
 
-
-// import ManageMembers from "../Pages/Dashboard/ManageMembers";
-// import MakeAnnouncement from "../Pages/Dashboard/MakeAnnouncement";
-// import AgreementRequests from "../Pages/Dashboard/AgreementRequests";
-// import ManageCoupons from "../Pages/Dashboard/ManageCoupons";
-// import AdminProfile from "../Pages/Dashboard/AdminProfile"; // চ্যালেঞ্জ টাস্ক
- // নতুন রোল-ভিত্তিক রাউট
-// import AdminRoute from "../Routes/AdminRoute"; // নতুন রোল-ভিত্তিক রাউট
+// import AdminRoute from "../Routes/AdminRoute";
 import DashboardLayout from "../DashboardLayout ";
 import MyProfile from "../Dashboard/MyProfile";
 import Announcements from "../Dashboard/Announcements";
@@ -87,120 +23,119 @@ import AgreementRequests from "../Pages/AgreementRequests";
 import ManageCoupons from "../Pages/ManageCoupons";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <RootLayout />,
-        children: [
-            {
-                index: true,
-                element: <HomePage />,
-            },
-            {
-                path: '/apartment',
-                element: <Apartment />
-            },
-            {
-                path: '/apply-agreement/:id',
-                element: (
-                    <PrivateRoute>
-                        <ApplyAgreement />
-                    </PrivateRoute>
-                ),
-            },
-        ],
-    },
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/register",
-        element: <Register />,
-    },
-    {
-        path: "/dashboard",
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/apartment",
+        element: <Apartment />,
+      },
+      {
+        path: "/apply-agreement/:id",
         element: (
-            <PrivateRoute> {/* ড্যাশবোর্ড অ্যাক্সেস করতে লগইন প্রয়োজন */}
-                <DashboardLayout />
-            </PrivateRoute>
+          <PrivateRoute>
+            <ApplyAgreement />
+          </PrivateRoute>
         ),
-        children: [
-            // User Dashboard Routes
-            {
-                path: "my-profile",
-                element: <MyProfile />,
-            },
-            {
-                path: "announcements",
-                element: <Announcements />,
-            },
-            // Member Dashboard Routes (only accessible to members)
-            {
-                path: "make-payment",
-                element: (
-                    <MemberRoute>
-                        <MakePayment />
-                    </MemberRoute>
-                ),
-            },
-            {
-                path: "payment-history",
-                element: (
-                    <MemberRoute>
-                        <PaymentHistory />
-                    </MemberRoute>
-                ),
-            },
-            // Admin Dashboard Routes (only accessible to admins)
-            {
-                path: "admin-profile",
-                element: (
-                    <AdminRoute>
-                        <AdminProfile />
-                    </AdminRoute>
-                ),
-            },
-            {
-                path: "manage-members",
-                element: (
-                    <AdminRoute>
-                        <ManageMembers />
-                    </AdminRoute>
-                ),
-            },
-            {
-                path: "make-announcement",
-                element: (
-                    <AdminRoute>
-                        <MakeAnnouncement />
-                    </AdminRoute>
-                ),
-            },
-            {
-                path: "agreement-requests",
-                element: (
-                    <AdminRoute>
-                        <AgreementRequests />
-                    </AdminRoute>
-                ),
-            },
-            {
-                path: "manage-coupons",
-                element: (
-                    <AdminRoute>
-                        <ManageCoupons />
-                    </AdminRoute>
-                ),
-            },
-            // Default dashboard route for authenticated users
-            {
-                index: true, // This will be the default route for /dashboard
-                element: <MyProfile />, // Or redirect based on role if preferred
-            },
-        ],
-    },
-    {
-        path: "*",
-        element: <ErrorPage />,
-    }
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        path: "my-profile",
+        element: <MyProfile />,
+      },
+      {
+        path: "announcements",
+        element: <Announcements />,
+      },
+
+      {
+        path: "make-payment",
+        element: (
+          <MemberRoute>
+            <MakePayment />
+          </MemberRoute>
+        ),
+      },
+      {
+        path: "payment-history",
+        element: (
+          <MemberRoute>
+            <PaymentHistory />
+          </MemberRoute>
+        ),
+      },
+
+      {
+        path: "admin-profile",
+        element: (
+          <AdminRoute>
+            <AdminProfile />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "manage-members",
+        element: (
+          <AdminRoute>
+            <ManageMembers />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "make-announcement",
+        element: (
+          <AdminRoute>
+            <MakeAnnouncement />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "agreement-requests",
+        element: (
+          <AdminRoute>
+            <AgreementRequests />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "manage-coupons",
+        element: (
+          <AdminRoute>
+            <ManageCoupons />
+          </AdminRoute>
+        ),
+      },
+
+      {
+        index: true,
+        element: <MyProfile />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
 ]);
